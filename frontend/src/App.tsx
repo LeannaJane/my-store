cc77ad5ff335332a5968e64f92dec584bc975fb6
LeannaJane <leannajane2002@outlook.com>
2025-08-16 13:54:27 +0100

    feat: loginm page/register

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import { RegisterPage } from "./Shopping/pages/SignUpPage";
 import { WelcomePage } from "./Shopping/pages/WelcomePage";
 
 
 const App = () => {
-  return <WelcomePage />;
+   return (
+    <Routes>
+      <Route path="/" element={<WelcomePage />} />
+      <Route path="/RegisterPage" element={<RegisterPage />} />
+    </Routes>
+  );
 };
 
 export default App;
--- frontend/src/Shopping/components/auth/Login.tsx
+++ frontend/src/Shopping/components/auth/Login.tsx
@@ -33,7 +33,7 @@ const Login = ({ onLoginSuccess }: LoginProps) => {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
+    <div className="flex items-center justify-center bg-gray-100">
       <form
         onSubmit={handleSubmit}
         className="bg-white p-6 rounded shadow-md w-full max-w-sm"
